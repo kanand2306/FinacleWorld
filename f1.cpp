ebe8041f58eb0eb6ac6470c5994345570f614694
AnandKumar <anandkumar@edgeverve.com>
2022-08-04 12:53:10 +0530

    UI update done

---
+++ f1.cpp
@@ -0,0 +1,7 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+cout<<"first file"<<endl;
+return 0;
+}
---
+++ f2.cpp
@@ -0,0 +1,7 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+cout<<"second file"<<endl;
+return 0;
+}
